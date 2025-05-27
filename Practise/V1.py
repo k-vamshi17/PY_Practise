1fb84d67b60ea232cc66ada8505f61247a19a2a1
Kuncha Vamshi Reddy <vamshireddy594@gmail.com>
2025-05-27 08:12:07 +0530

    v1

--- Practise/V1.py
+++ Practise/V1.py
@@ -1,3 +1,69 @@
+'''write a python code without range
+1
+1 2 
+1 2 3
+1 2 3 4 '''
+
+row = int(input("Value:"))
+i=1
+while i<=row:
+    j=1
+    while j<=i:
+        print(j, end=' ')
+        j +=1
+    print()
+    i +=1
+
+## Count how many vowels (a, e, i, o, u) exist in a given string (case-insensitive).
+lst=['a','e','i','o','u']
+lst1=[]
+word=list(input("Vowels :"))
+for i in lst:
+    if i in word:
+        lst1.append(i)
+print(len(lst1))
+
+# Sort a List Without sort()
+def sort_list(lst):
+    n = len(lst)
+    i = 0
+    while i < n:
+        j = 0
+        while j < (n - i - 1):
+            if lst[j] > lst[j + 1]:
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+            j += 1
+        i += 1
+    return lst
+
+numbers = [0,5,2]
+print("sorted list ",sort_list(numbers))
+
+# Find the Second-Largest Number
+def sort_list(lst):
+    n = len(lst)
+    i = 0
+    while i < n:
+        j = 0
+        while j < (n - i - 1):
+            if lst[j] > lst[j + 1]:
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+            j += 1
+        i += 1
+    return lst
+
+numbers = [0,5,2]
+sorted=sort_list(numbers)
+print("second largest",sorted[-2])
+
+
+# Reverse Words in a String
+def rev(string):
+    print(string[::-1])
+    return rev
+lst="hello"
+rev(lst)
+
 # 1. Merge & Sort Two Lists without using sorted()
 
 # Define the first list
@@ -95,4 +161,3 @@ def are_anagrams(str1, str2):
 # Example usage:
 print(are_anagrams("listen", "silent"))  # True
 print(are_anagrams("hello", "world"))    # False
-
