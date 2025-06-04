3e309a557e3da4848204901a6b0d2073f3177d82
Kuncha Vamshi Reddy <vamshireddy594@gmail.com>
2025-06-04 21:09:32 +0530

    leetcode

--- Practise/leetcode.py
+++ Practise/leetcode.py
@@ -267,24 +267,24 @@ class Solution:
         return cnt
 
 # -------------------------------------------------------------------------------
-class Solution(object):
-    def answerString(self, word, numFriends):
-        """
-        :type word: str
-        :type numFriends: int
-        :rtype: str
-        """
-        if numFriends == 1:
-            return word
+# class Solution(object):
+#     def answerString(self, word, numFriends):
+#         """
+#         :type word: str
+#         :type numFriends: int
+#         :rtype: str
+#         """
+#         if numFriends == 1:
+#             return word
 
-        n = len(word)
-        length = n - numFriends + 1
-        max_char = max(word)
-        result = ""
+#         n = len(word)
+#         length = n - numFriends + 1
+#         max_char = max(word)
+#         result = ""
 
-        for i in range(n):
-            if word[i] == max_char:
-                substr = word[i:i + length]
-                result = max(result, substr)
+#         for i in range(n):
+#             if word[i] == max_char:
+#                 substr = word[i:i + length]
+#                 result = max(result, substr)
 
-        return result
+#         return result
