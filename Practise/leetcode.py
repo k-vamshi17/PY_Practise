322eb2084c0bcf6069575e1e243222628c2fd7f2
Kuncha Vamshi Reddy <vamshireddy594@gmail.com>
2025-06-04 11:49:31 +0530

    leetcode

--- Practise/leetcode.py
+++ Practise/leetcode.py
@@ -265,3 +265,26 @@ class Solution:
             if status[i]==3:
                 cnt+=dfs(i)
         return cnt
+
+# -------------------------------------------------------------------------------
+class Solution(object):
+    def answerString(self, word, numFriends):
+        """
+        :type word: str
+        :type numFriends: int
+        :rtype: str
+        """
+        if numFriends == 1:
+            return word
+
+        n = len(word)
+        length = n - numFriends + 1
+        max_char = max(word)
+        result = ""
+
+        for i in range(n):
+            if word[i] == max_char:
+                substr = word[i:i + length]
+                result = max(result, substr)
+
+        return result
